1624a6a1732cf8b5cf69514d50ee3b055ed7dc7e
Carlos C Soto <eclipxe13@gmail.com>
2016-06-09 18:33:09 -0500

    Use AbstractLogger

--- tests/EngineWorks/DBAL/Tests/Sample/ArrayLogger.php
+++ tests/EngineWorks/DBAL/Tests/Sample/ArrayLogger.php
@@ -1,52 +1,12 @@
 <?php namespace EngineWorks\DBAL\Tests\Sample;
 
-use Psr\Log\LoggerInterface;
+use Psr\Log\AbstractLogger;
 
-class ArrayLogger implements LoggerInterface
+class ArrayLogger extends AbstractLogger
 {
     /** @var array */
     private $logs = [];
     
-    public function emergency($message, array $context = [])
-    {
-        $this->log('message', $message, $context);
-    }
-
-    public function alert($message, array $context = [])
-    {
-        $this->log('alert', $message, $context);
-    }
-
-    public function critical($message, array $context = [])
-    {
-        $this->log('critical', $message, $context);
-    }
-
-    public function error($message, array $context = [])
-    {
-        $this->log('error', $message, $context);
-    }
-
-    public function warning($message, array $context = [])
-    {
-        $this->log('warning', $message, $context);
-    }
-
-    public function notice($message, array $context = [])
-    {
-        $this->log('notice', $message, $context);
-    }
-
-    public function info($message, array $context = [])
-    {
-        $this->log('info', $message, $context);
-    }
-
-    public function debug($message, array $context = [])
-    {
-        $this->log('debug', $message, $context);
-    }
-
     public function log($level, $message, array $context = [])
     {
         $this->logs[$level][] = [
