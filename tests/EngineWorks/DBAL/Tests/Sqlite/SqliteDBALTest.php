a244c50d1165ec28bcd7ac6ad1f96ef24dae5877
Carlos C Soto <eclipxe13@gmail.com>
2016-06-09 13:01:51 -0500

    WIP: Improve tests

--- tests/EngineWorks/DBAL/Tests/Sqlite/SqliteDBALTest.php
+++ tests/EngineWorks/DBAL/Tests/Sqlite/SqliteDBALTest.php
@@ -3,9 +3,14 @@
 use EngineWorks\DBAL\DBAL;
 use EngineWorks\DBAL\Factory;
 use EngineWorks\DBAL\Settings;
+use EngineWorks\DBAL\Tests\Sample\ArrayLogger;
+use Psr\Log\NullLogger;
 
 class SqliteDBALTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var Factory */
+    private $factory;
+
     /** @var DBAL */
     private $dbal;
 
@@ -16,25 +21,74 @@ class SqliteDBALTest extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
         if ($this->dbal === null) {
-            $factory = new Factory('EngineWorks\DBAL\Sqlite');
-            $this->settings = $factory->settings([
+            $this->factory = new Factory('EngineWorks\DBAL\Sqlite');
+            $this->settings = $this->factory->settings([
                 'filename' => ':memory:'
             ]);
-            $this->dbal = $factory->dbal($this->settings);
+            $this->dbal = $this->factory->dbal($this->settings);
             $this->dbal->connect();
         }
     }
 
+    /** @return ArrayLogger */
+    protected function dbalGetArrayLogger()
+    {
+        return $this->dbal->getLogger();
+    }
+
+    protected function dbalSetArrayLogger()
+    {
+        $this->dbal->setLogger(new ArrayLogger());
+    }
+
+    protected function dbalUnsetArrayLogger()
+    {
+        $this->dbal->setLogger(new NullLogger());
+    }
 
     public function testDisconnect()
     {
         $this->assertTrue($this->dbal->isConnected());
+        $this->dbalSetArrayLogger();
         $this->dbal->disconnect();
+        $expectedLogs = [
+            'info: -- Disconnection'
+        ];
         $this->assertFalse($this->dbal->isConnected());
+        $this->assertSame($expectedLogs, $this->dbalGetArrayLogger()->allMessages());
+        $this->dbal->disconnect();
+        $this->assertSame(
+            $expectedLogs,
+            $this->dbalGetArrayLogger()->allMessages(),
+            'Disconnect create two logs instead of only one'
+        );
     }
-    
-    public function testConnect()
+
+    public function testConnectReturnFalseWhenCannotConnect()
     {
-        $this->dbal->connect();
+        $dbal = $this->factory->dbal($this->factory->settings([
+            'filename' => 'non-existent',
+            'flags' => 0,
+        ]));
+        $logger = new ArrayLogger();
+        $dbal->setLogger($logger);
+        $this->assertFalse($dbal->connect());
+        $expectedLogs = [
+            'info: -- Connection fail',
+            'error: Cannot create SQLite3 object: Unable to open database: out of memory'
+        ];
+        $this->assertSame($expectedLogs, $logger->allMessages());
+    }
+
+    public function testConnectLogger()
+    {
+        $this->assertTrue($this->dbal->isConnected());
+        $this->dbal->disconnect();
+        $this->dbalSetArrayLogger();
+        $this->assertTrue($this->dbal->connect());
+        $expectedLogs = [
+            'info: -- Connection success'
+        ];
+        $this->assertSame($expectedLogs, $this->dbalGetArrayLogger()->allMessages());
     }
 }
