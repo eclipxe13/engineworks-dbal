20202b30dc25f6d432c878693582ca00b2a08fc8
Carlos C Soto <eclipxe13@gmail.com>
2016-07-25 18:49:14 -0500

    Append trail ";"

--- sources/EngineWorks/DBAL/Traits/MethodSqlLimit.php
+++ sources/EngineWorks/DBAL/Traits/MethodSqlLimit.php
@@ -10,7 +10,8 @@ trait MethodSqlLimit
         $recordsPerPage = max(1, (int) $recordsPerPage);
         $query = rtrim($query, "; \t\n\r\0\x0B")
             . ' LIMIT ' . $this->sqlQuote($recordsPerPage, CommonTypes::TINT)
-            . ' OFFSET ' . $this->sqlQuote($recordsPerPage * $requestedPage, CommonTypes::TINT) ;
+            . ' OFFSET ' . $this->sqlQuote($recordsPerPage * $requestedPage, CommonTypes::TINT)
+            . ';';
         return $query;
     }
 }
