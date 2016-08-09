951c210a16cb73e9d0f682ab5f96945503db05aa
Carlos C Soto <eclipxe13@gmail.com>
2016-08-09 10:19:19 -0500

    Create test to sqlite result

---
+++ tests/EngineWorks/DBAL/Tests/Sqlite/ResultTest.php
@@ -0,0 +1,106 @@
+<?php
+namespace EngineWorks\DBAL\Tests\Sqlite;
+
+use EngineWorks\DBAL\CommonTypes;
+use EngineWorks\DBAL\Iterators\ResultIterator;
+use EngineWorks\DBAL\Result;
+use EngineWorks\DBAL\Tests\TestCaseWithSqliteDatabase;
+
+class ResultTest extends TestCaseWithSqliteDatabase
+{
+    /** @var Result */
+    private $result;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        if (! ($this->result instanceof Result)) {
+            $this->result = $this->dbal->queryResult('SELECT * FROM albums WHERE (albumid between 1 and 3);');
+        }
+    }
+
+    public function testResultCount()
+    {
+        $this->assertSame(3, $this->result->resultCount());
+    }
+
+    public function testQueryResult()
+    {
+        $this->assertInstanceOf(Result::class, $this->result);
+        $this->assertInstanceOf(\Countable::class, $this->result);
+        $this->assertInstanceOf(\Traversable::class, $this->result);
+        $this->assertCount(3, $this->result);
+    }
+
+    public function testIterator()
+    {
+        $fist = $this->getForEach();
+        $this->assertCount(3, $fist);
+        $second = $this->getForEach();
+        $this->assertCount(3, $second);
+        $this->assertEquals($fist, $second);
+    }
+
+    public function testIteratorAfterMovingTheFirstRecord()
+    {
+        $this->result->fetchRow();
+        $contents = $this->getForEach();
+        $this->assertCount(3, $contents);
+    }
+
+    public function testMoveOffSet()
+    {
+        $current = $this->getForEach();
+        $this->assertSame($current[0], $this->result->fetchRow(), 'After for each fetch must return the first row');
+        $this->assertTrue($this->result->moveTo(0));
+        $this->assertSame($current[0], $this->result->fetchRow(), 'After move to index 0 fetch must return first row');
+        $this->assertTrue($this->result->moveTo(2));
+        $this->assertSame($current[2], $this->result->fetchRow(), 'After move to index 2 fetch must return last row');
+        $this->assertTrue($this->result->moveTo(1));
+        $this->assertSame($current[1], $this->result->fetchRow(), 'After move to index 1 fetch must return second row');
+    }
+
+    public function testGetFields()
+    {
+        // cannot get (yet) the table from a query
+        $expected = [
+            [
+                'name' => 'albumid',
+                'commontype' => CommonTypes::TINT,
+                'table' => '',
+                'flags' => null,
+            ],
+            [
+                'name' => 'title',
+                'commontype' => CommonTypes::TTEXT,
+                'table' => '',
+                'flags' => null,
+            ],
+        ];
+        $this->result->fetchRow();
+        $this->assertEquals($expected, $this->result->getFields());
+    }
+
+    public function testGetIdFields()
+    {
+        $this->result->fetchRow();
+        $this->assertEquals(false, $this->result->getIdFields(), 'Cannot get (yet) the Id Fields from a query');
+    }
+
+    public function testGetIterator()
+    {
+        $iterator = $this->result->getIterator();
+        $this->assertInstanceOf(\Iterator::class, $iterator);
+        $this->assertInstanceOf(ResultIterator::class, $iterator);
+    }
+
+    private function getForEach()
+    {
+        $array = [];
+        foreach ($this->result as $values) {
+            $this->assertInternalType('array', $values);
+            $array[] = $values;
+        }
+        return $array;
+    }
+}
