e6fad84682bdebc3a7352893b02e4a45a0a485d7
Carlos C Soto <eclipxe13@gmail.com>
2018-01-23 11:27:38 -0600

    Test against phpunit version, not php version

--- tests/EngineWorks/DBAL/Tests/TransactionsWithExceptionsTestTrait.php
+++ tests/EngineWorks/DBAL/Tests/TransactionsWithExceptionsTestTrait.php
@@ -3,6 +3,7 @@ namespace EngineWorks\DBAL\Tests;
 
 use EngineWorks\DBAL\DBAL;
 use PHPUnit\Framework\Error\Notice;
+use PHPUnit\Runner\Version;
 
 /* @var $this \EngineWorks\DBAL\Tests\TestCaseWithDatabase */
 
@@ -11,20 +12,27 @@ trait TransactionsWithExceptionsTestTrait
     /** @return DBAL */
     abstract protected function getDbal();
 
+    private function checkPhpUnitVersion($minimalVersion)
+    {
+        $phpUnitVersion = '5.6';
+        if (class_exists(Version::class)) {
+            $phpUnitVersion = Version::series();
+        }
+        if (version_compare($phpUnitVersion, $minimalVersion, '<')) {
+            $this->markTestSkipped(sprintf('This test only runs on phpunit %s or higher', $minimalVersion));
+        }
+    }
+
     public function testCommitThrowsWarningWithOutBegin()
     {
-        if (version_compare(PHP_VERSION, '7.0', '<')) {
-            $this->markTestSkipped('This test only runs on php 7.0 or higher');
-        }
+        $this->checkPhpUnitVersion('6.0');
         $this->expectException(Notice::class);
         $this->getDbal()->transCommit();
     }
 
     public function testRollbackThrowsWarningWithOutBegin()
     {
-        if (version_compare(PHP_VERSION, '7.0', '<')) {
-            $this->markTestSkipped('This test only runs on php 7.0 or higher');
-        }
+        $this->checkPhpUnitVersion('6.0');
         $this->expectException(Notice::class);
         $this->getDbal()->transRollback();
     }
